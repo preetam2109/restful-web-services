92d85ce2bc953cca05d4d2cd4ae1562cdb9cb96b
Preetam <nihal@Nihal.local>
2023-08-26 21:09:15 +0530

    initial commit

---
+++ src/main/java/com/preetam/restfulwebservicesv3/RestfulWebServicesV3Application.java
@@ -0,0 +1,13 @@
+package com.preetam.restfulwebservicesv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/preetam/restfulwebservicesv3/RestfulWebServicesV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preetam.restfulwebservicesv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
